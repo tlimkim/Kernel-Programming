4816ab4a5f626fa574606a19f4ce39c700febed2
tlimkim <limkim4233@gmail.com>
2018-02-20 13:23:22 +0900

    Added ex4 task1

---
+++ ex4_memory_management/taeklim/mod_task/.tmp_versions/process_task_mod.mod
@@ -0,0 +1,3 @@
+/home/tlimkim/workspace/kernel_programming/ex4_memory_management/taeklim/mod_task/process_task_mod.ko
+/home/tlimkim/workspace/kernel_programming/ex4_memory_management/taeklim/mod_task/process_task_mod.o
+
---
+++ ex4_memory_management/taeklim/mod_task/process_task_mod.c
@@ -0,0 +1,36 @@
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ajou.ac.kr");
+
+//struct task_struct *proc = current;
+
+static int pid = 1; //
+
+module_param (pid, int, 0);
+
+static int __init proc_init (void)
+{
+    printk(KERN_ALERT "Module Started \n");
+    printk("PID: %d \n", pid);
+    
+    struct task_struct *task;
+
+    task = pid_task(find_vpid(pid), PIDTYPE_PID);
+
+    printk("Process name: %s \n", task->comm);
+
+    return 0;
+}
+
+static void __exit proc_exit (void)
+{
+
+    printk(KERN_ALERT "Module Removed \n");
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
