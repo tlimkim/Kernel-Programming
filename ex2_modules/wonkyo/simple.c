bab2930f326a7f68008de052ab2839a463b82a08
Wonkyo Choe <heysid@ajou.ac.kr>
2018-08-24 14:25:42 +0900

    added dir: wonkyo

---
+++ ex2_modules/wonkyo/simple.c
@@ -0,0 +1,20 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("wonkyo");
+
+static int __init hello_module(void)
+{
+    printk(KERN_ALERT "Hello Module! \n");
+    return 0;
+}
+
+static void __exit bye_module(void)
+{
+    printk(KERN_ALERT "Bye Module! \n");
+}
+
+module_init(hello_module);
+module_exit(bye_module);
